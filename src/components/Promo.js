46708af096911f49a12564976ab90d7bd273661d
Andrey Willian <andreywillianlemos@hotmail.com>
2022-04-13 17:21:47 -0300

    first commit

---
+++ src/components/Promo.js
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core"
+
+export const promo = () => {
+    return (
+        <Typography>
+            Acima de R$1.000 o frete é por nossa conta, aproveite!
+        </Typography>
+    )
+}
---
+++ src/components/pages/Header.js
@@ -0,0 +1,15 @@
+import { Box } from "@material-ui/core";
+import CardMedia from '@mui/material/CardMedia';
+import LazzarettiLogo from '../assets/LazzarettiLogo.png';
+import React from "react";
+
+function Header() {
+    return (
+        <Box sx={{ width: '100%', bgColor: 'blue' }}>
+            
+        </Box>
+    );
+
+}
+
+export default Header;
